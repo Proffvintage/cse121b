a94bab4dc69c677c5762e49ee78631b8f01a4f51
Proffvintage <145012191+Proffvintage@users.noreply.github.com>
2024-01-17 19:18:13 +0100

    Add files via upload

---
+++ scripts/w02-task.js
@@ -0,0 +1,46 @@
+/* W02-Task - Profile Home Page */
+
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName='Samuel Markson';
+let currentYear='2024';
+let profilePic='images/pic.png';
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imgElement= document.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML= `<strong>${fullName} </strong>`;
+yearElement.testContent= currentYear;
+imgElement.setAttribute('src', profilePic);
+imgElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+
+
+
+
+/* Step 5 - Array */
+const favoriteFoods = ['Rice', 'Beans', 'Pasta', 'Chicken pie'];
+foodElement.innerHTML=favoriteFoods
+const favFood = ['Mango'];
+favoriteFoods.push(favFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift(favFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+
+
+
+
